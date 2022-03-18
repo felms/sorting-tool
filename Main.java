981f6f5d644dcf5cc3c8b82bce6f667bf605072c
Felipe <felipemoreira1@gmail.com>
2022-03-18 14:20:09 -0300

    Stage 2/6: Not just numbers

--- Main.java
+++ Main.java
@@ -1,9 +1,36 @@
 import java.util.*;
 
 public class Main {
-    public static void main(final String[] args) {
-        Scanner scanner = new Scanner(System.in);
 
+    private static Scanner scanner;
+    public static void main(final String[] args) {
+        scanner = new Scanner(System.in);
+
+        String datatype; 
+
+        if (args.length == 2) {
+            datatype = args[1];
+        } else {
+            datatype = "word";
+        }
+
+        switch(datatype) {
+            case "long":
+                readLongs();
+                break;
+            case "line":
+                readLines();
+                break;
+            case "word":
+                readWords();
+                break;
+        }
+
+
+        scanner.close();
+    }
+
+    private static void readLongs() {
         Map<Long, Integer> numberFrequency = new Hashtable<>();
         Long greatestNumber = Long.MIN_VALUE;
         int countNumbers = 0;
@@ -24,7 +51,61 @@ public class Main {
         }
 
         System.out.printf("Total numbers: %d.\n" +
-                "The greatest number: %d (%d time(s)).",
+                "The greatest number: %d ( %d time(s)).",
                 countNumbers, greatestNumber, numberFrequency.get(greatestNumber));
+    }
+
+    private static void readLines() {
+        Map<String, Integer> lineFrequency = new Hashtable<>();
+        String  longestLine = "";
+        int countLines = 0;
+
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            if (lineFrequency.containsKey(line)) {
+                int frequency = lineFrequency.get(line);
+                frequency++;
+                lineFrequency.replace(line, frequency);
+            } else {
+                lineFrequency.put(line, 1);
+            }
+            if (line.length() > longestLine.length()) {
+                longestLine = line;
+            }
+            countLines++;
+        }
+
+        int percentage = (int) (((double) lineFrequency.get(longestLine) / (double)countLines) * 100);
+        System.out.println("Total lines: " + countLines + ".\n" +
+                "The longest line:\n" + 
+                longestLine + "\n" + 
+                "(" + lineFrequency.get(longestLine) + " time(s), " + 
+                 percentage + "%).");
+    }
+
+    private static void readWords() {
+        Map<String, Integer> wordFrequency = new Hashtable<>();
+        String  longestWord = "";
+        int countWords = 0;
+
+        while (scanner.hasNext()) {
+            String word = scanner.next();
+            if (wordFrequency.containsKey(word)) {
+                int frequency = wordFrequency.get(word);
+                frequency++;
+                wordFrequency.replace(word, frequency);
+            } else {
+                wordFrequency.put(word, 1);
+            }
+            if (word.length() > longestWord.length()) {
+                longestWord = word;
+            }
+            countWords++;
+        }
+
+        int percentage = (int)(((double) wordFrequency.get(longestWord) / (double) countWords) * 100);
+        System.out.println("Total words: " + countWords + ".\n" +
+                "The longest word: " + longestWord + " (" + wordFrequency.get(longestWord) + " time(s), " + 
+                 percentage + "%).");
     }
 }
